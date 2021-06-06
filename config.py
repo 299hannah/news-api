0e898939c9b759343c9225dc0a8a3bb31109fa04
299hannah <hannah.wambui@student.moringaschool.com>
2021-06-07 02:22:05 +0300

    config

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-
-    pass
+    SPECIFI_API_URL
+    
 class ProdConfig(Config):
     '''
     production configuration
