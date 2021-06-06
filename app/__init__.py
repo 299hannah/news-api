682c9f150f8b495ee4743b22c1b3b52d7305a4b1
299hannah <hannah.wambui@student.moringaschool.com>
2021-06-07 00:11:06 +0300

    models.py and init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,27 @@
 from flask import Flask 
+from config import config_options
+from flask_bootstrap import Bootstrap
 
-#initializing application
-app= Flask(__name__)
+#initializing bootstrap extension
+bootstrap = Bootstrap()
 
-#Set up configurations
-app.config.from_object(DevConfig)
-from app import views
+def create_app(config_name):
+
+    #initializing application
+    app= Flask(__name__,instance_relative_config =True)
+
+    #creating configurations
+    app.config.from_object(config_options[config_name])
+    
+    #initialize flask extensions
+    bootstrap.init_app(app)
+
+    #registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    #Set up configurations
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
--- app/models.py
+++ app/models.py
@@ -1,6 +1,21 @@
 class News_Article:
     '''
+    defines article objects
     '''
+
+    def __init__(self,title,description,url,image,published_on):
+        self.title =title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.published_on=published_on
+
 class News_source:
     '''
+    defines source objects
     '''
+    def __init__(self,news_id,name,description):
+        self.news_id = news_id
+        self.name = name
+        self.description = description
+        
